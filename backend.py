44386d2ff2a8e1721fa2dac2495f7c5ebb32125b
Albert <ayang04@zohomail.com>
2025-11-29 19:17:52 -0800

    1

--- backend.py
+++ backend.py
@@ -106,5 +106,9 @@ def search_posts():
         return jsonify({"error": "Internal server error"}), 500
 
    
+#if __name__ == "__main__":
+   # app.run(debug=True, port=8000) # debug=True enables debug mode and auto-reloading
+
 if __name__ == "__main__":
-    app.run(debug=True, port=8000) # debug=True enables debug mode and auto-reloading
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=False)  
